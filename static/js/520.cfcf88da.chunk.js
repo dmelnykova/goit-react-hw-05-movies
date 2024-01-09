f5ed58390c8bca879cb4afb18e03ca8402fc2b82
dmelnykova <130089251+dmelnykova@users.noreply.github.com>
2024-01-09 18:01:15 +0000

    Deploying to gh-pages from @ dmelnykova/goit-react-hw-05-movies@c04b9bb1883d112ab6be8728497108ab2dae4711 🚀

---
+++ static/js/520.cfcf88da.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(87),c=n(184);function o(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("b",{children:["Pages not found, back ",(0,c.jsx)(r.rU,{to:"/",children:"link"})," to Home"]})})}}}]);
+//# sourceMappingURL=520.cfcf88da.chunk.js.map
